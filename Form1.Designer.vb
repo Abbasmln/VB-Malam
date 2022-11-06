96e939e0b9443c69bd6708c1d0b32436a8e3e3ff
Abbasmln <117564999+Abbasmln@users.noreply.github.com>
2022-11-06 17:59:06 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,484 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.BtnClear = New System.Windows.Forms.Button()
+        Me.txtGrade = New System.Windows.Forms.TextBox()
+        Me.txtNilai = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.BtnHitung = New System.Windows.Forms.Button()
+        Me.txtUAS = New System.Windows.Forms.TextBox()
+        Me.txtUTS = New System.Windows.Forms.TextBox()
+        Me.txtTugas = New System.Windows.Forms.TextBox()
+        Me.txtKehadiran = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtNPM = New System.Windows.Forms.TextBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtBobot = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.txtSKS = New System.Windows.Forms.TextBox()
+        Me.BtnIPS = New System.Windows.Forms.Button()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.txtIPS = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.txtjumIPS = New System.Windows.Forms.TextBox()
+        Me.Semester = New System.Windows.Forms.Label()
+        Me.txtSemester = New System.Windows.Forms.TextBox()
+        Me.BtnIPK = New System.Windows.Forms.Button()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.txtHasilIPK = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(473, 575)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(130, 44)
+        Me.BtnExit.TabIndex = 39
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'BtnClear
+        '
+        Me.BtnClear.Location = New System.Drawing.Point(137, 575)
+        Me.BtnClear.Name = "BtnClear"
+        Me.BtnClear.Size = New System.Drawing.Size(130, 44)
+        Me.BtnClear.TabIndex = 38
+        Me.BtnClear.Text = "Clear"
+        Me.BtnClear.UseVisualStyleBackColor = True
+        '
+        'txtGrade
+        '
+        Me.txtGrade.Location = New System.Drawing.Point(303, 496)
+        Me.txtGrade.Name = "txtGrade"
+        Me.txtGrade.Size = New System.Drawing.Size(99, 26)
+        Me.txtGrade.TabIndex = 37
+        '
+        'txtNilai
+        '
+        Me.txtNilai.Location = New System.Drawing.Point(111, 496)
+        Me.txtNilai.Name = "txtNilai"
+        Me.txtNilai.Size = New System.Drawing.Size(99, 26)
+        Me.txtNilai.TabIndex = 36
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(327, 457)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(54, 20)
+        Me.Label9.TabIndex = 35
+        Me.Label9.Text = "Grade"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(121, 457)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(78, 20)
+        Me.Label8.TabIndex = 34
+        Me.Label8.Text = "Nilai Akhir"
+        '
+        'BtnHitung
+        '
+        Me.BtnHitung.Location = New System.Drawing.Point(491, 230)
+        Me.BtnHitung.Name = "BtnHitung"
+        Me.BtnHitung.Size = New System.Drawing.Size(195, 184)
+        Me.BtnHitung.TabIndex = 33
+        Me.BtnHitung.Text = "Hitung"
+        Me.BtnHitung.UseVisualStyleBackColor = True
+        '
+        'txtUAS
+        '
+        Me.txtUAS.Location = New System.Drawing.Point(228, 388)
+        Me.txtUAS.Name = "txtUAS"
+        Me.txtUAS.Size = New System.Drawing.Size(207, 26)
+        Me.txtUAS.TabIndex = 32
+        '
+        'txtUTS
+        '
+        Me.txtUTS.Location = New System.Drawing.Point(228, 335)
+        Me.txtUTS.Name = "txtUTS"
+        Me.txtUTS.Size = New System.Drawing.Size(207, 26)
+        Me.txtUTS.TabIndex = 31
+        '
+        'txtTugas
+        '
+        Me.txtTugas.Location = New System.Drawing.Point(228, 281)
+        Me.txtTugas.Name = "txtTugas"
+        Me.txtTugas.Size = New System.Drawing.Size(207, 26)
+        Me.txtTugas.TabIndex = 30
+        '
+        'txtKehadiran
+        '
+        Me.txtKehadiran.Location = New System.Drawing.Point(228, 230)
+        Me.txtKehadiran.Name = "txtKehadiran"
+        Me.txtKehadiran.Size = New System.Drawing.Size(207, 26)
+        Me.txtKehadiran.TabIndex = 29
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(107, 394)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(43, 20)
+        Me.Label7.TabIndex = 28
+        Me.Label7.Text = "UAS"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(107, 341)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 20)
+        Me.Label6.TabIndex = 27
+        Me.Label6.Text = "UTS"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(107, 287)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 20)
+        Me.Label5.TabIndex = 26
+        Me.Label5.Text = "Tugas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(107, 236)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 20)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Text = "Kehadiran"
+        '
+        'txtNPM
+        '
+        Me.txtNPM.Location = New System.Drawing.Point(228, 171)
+        Me.txtNPM.Name = "txtNPM"
+        Me.txtNPM.Size = New System.Drawing.Size(458, 26)
+        Me.txtNPM.TabIndex = 24
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(228, 119)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(458, 26)
+        Me.txtNama.TabIndex = 23
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(107, 177)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 20)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "NPM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(107, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 21
+        Me.Label2.Text = "Nama"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(130, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(496, 37)
+        Me.Label1.TabIndex = 20
+        Me.Label1.Text = "Penilaian Grade Nilai Mata Kuliah"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBox1.Location = New System.Drawing.Point(716, 31)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(10, 621)
+        Me.PictureBox1.TabIndex = 40
+        Me.PictureBox1.TabStop = False
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(991, 47)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(67, 37)
+        Me.Label10.TabIndex = 41
+        Me.Label10.Text = "IPS"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(520, 457)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(52, 20)
+        Me.Label11.TabIndex = 42
+        Me.Label11.Text = "Bobot"
+        '
+        'txtBobot
+        '
+        Me.txtBobot.Location = New System.Drawing.Point(504, 496)
+        Me.txtBobot.Name = "txtBobot"
+        Me.txtBobot.Size = New System.Drawing.Size(99, 26)
+        Me.txtBobot.TabIndex = 43
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(756, 119)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(382, 20)
+        Me.Label12.TabIndex = 44
+        Me.Label12.Text = "Jumlah Nilai Bobot * SKS pada setiap mata pelajaran"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(760, 154)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(204, 26)
+        Me.txtTotal.TabIndex = 45
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(756, 196)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(96, 20)
+        Me.Label13.TabIndex = 46
+        Me.Label13.Text = "Jumlah SKS"
+        '
+        'txtSKS
+        '
+        Me.txtSKS.Location = New System.Drawing.Point(760, 230)
+        Me.txtSKS.Name = "txtSKS"
+        Me.txtSKS.Size = New System.Drawing.Size(204, 26)
+        Me.txtSKS.TabIndex = 47
+        '
+        'BtnIPS
+        '
+        Me.BtnIPS.Location = New System.Drawing.Point(792, 287)
+        Me.BtnIPS.Name = "BtnIPS"
+        Me.BtnIPS.Size = New System.Drawing.Size(130, 44)
+        Me.BtnIPS.TabIndex = 48
+        Me.BtnIPS.Text = "Hitung"
+        Me.BtnIPS.UseVisualStyleBackColor = True
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(1103, 171)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(84, 32)
+        Me.Label14.TabIndex = 49
+        Me.Label14.Text = "Hasil"
+        '
+        'txtIPS
+        '
+        Me.txtIPS.Location = New System.Drawing.Point(1049, 230)
+        Me.txtIPS.Name = "txtIPS"
+        Me.txtIPS.Size = New System.Drawing.Size(204, 26)
+        Me.txtIPS.TabIndex = 50
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(991, 377)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(67, 37)
+        Me.Label15.TabIndex = 51
+        Me.Label15.Text = "IPK"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(756, 433)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(190, 20)
+        Me.Label16.TabIndex = 52
+        Me.Label16.Text = "Jumlah IPS per Semester"
+        '
+        'txtjumIPS
+        '
+        Me.txtjumIPS.Location = New System.Drawing.Point(760, 476)
+        Me.txtjumIPS.Name = "txtjumIPS"
+        Me.txtjumIPS.Size = New System.Drawing.Size(204, 26)
+        Me.txtjumIPS.TabIndex = 53
+        '
+        'Semester
+        '
+        Me.Semester.AutoSize = True
+        Me.Semester.Location = New System.Drawing.Point(756, 529)
+        Me.Semester.Name = "Semester"
+        Me.Semester.Size = New System.Drawing.Size(190, 20)
+        Me.Semester.TabIndex = 54
+        Me.Semester.Text = "Jumlah IPS per Semester"
+        '
+        'txtSemester
+        '
+        Me.txtSemester.Location = New System.Drawing.Point(760, 575)
+        Me.txtSemester.Name = "txtSemester"
+        Me.txtSemester.Size = New System.Drawing.Size(204, 26)
+        Me.txtSemester.TabIndex = 55
+        '
+        'BtnIPK
+        '
+        Me.BtnIPK.Location = New System.Drawing.Point(1079, 445)
+        Me.BtnIPK.Name = "BtnIPK"
+        Me.BtnIPK.Size = New System.Drawing.Size(130, 44)
+        Me.BtnIPK.TabIndex = 56
+        Me.BtnIPK.Text = "Hitung"
+        Me.BtnIPK.UseVisualStyleBackColor = True
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label17.Location = New System.Drawing.Point(1103, 517)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(84, 32)
+        Me.Label17.TabIndex = 57
+        Me.Label17.Text = "Hasil"
+        '
+        'txtHasilIPK
+        '
+        Me.txtHasilIPK.Location = New System.Drawing.Point(1049, 575)
+        Me.txtHasilIPK.Name = "txtHasilIPK"
+        Me.txtHasilIPK.Size = New System.Drawing.Size(204, 26)
+        Me.txtHasilIPK.TabIndex = 58
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1401, 682)
+        Me.Controls.Add(Me.txtHasilIPK)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.BtnIPK)
+        Me.Controls.Add(Me.txtSemester)
+        Me.Controls.Add(Me.Semester)
+        Me.Controls.Add(Me.txtjumIPS)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.txtIPS)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.BtnIPS)
+        Me.Controls.Add(Me.txtSKS)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.txtBobot)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.BtnClear)
+        Me.Controls.Add(Me.txtGrade)
+        Me.Controls.Add(Me.txtNilai)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.BtnHitung)
+        Me.Controls.Add(Me.txtUAS)
+        Me.Controls.Add(Me.txtUTS)
+        Me.Controls.Add(Me.txtTugas)
+        Me.Controls.Add(Me.txtKehadiran)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtNPM)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents txtGrade As TextBox
+    Friend WithEvents txtNilai As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents BtnHitung As Button
+    Friend WithEvents txtUAS As TextBox
+    Friend WithEvents txtUTS As TextBox
+    Friend WithEvents txtTugas As TextBox
+    Friend WithEvents txtKehadiran As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtNPM As TextBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txtBobot As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents txtSKS As TextBox
+    Friend WithEvents BtnIPS As Button
+    Friend WithEvents Label14 As Label
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents txtjumIPS As TextBox
+    Friend WithEvents Semester As Label
+    Friend WithEvents txtSemester As TextBox
+    Friend WithEvents BtnIPK As Button
+    Friend WithEvents Label17 As Label
+    Friend WithEvents txtHasilIPK As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        txtNama.Text = " "
+        txtNPM.Text = " "
+        txtKehadiran.Text = " "
+        txtTugas.Text = " "
+        txtUTS.Text = " "
+        txtUAS.Text = " "
+        txtNilai.Text = " "
+        txtGrade.Text = " "
+        txtBobot.Text = " "
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        End
+    End Sub
+
+    Private Sub BtnHitung_Click(sender As Object, e As EventArgs) Handles BtnHitung.Click
+        Dim Nama As String
+        Dim NPM As String
+        Dim Kehadiran As Double
+        Dim Tugas As Double
+        Dim UTS As Double
+        Dim UAS As Double
+        Dim Nilai As Double
+        Dim Grade As String
+
+        Nama = txtNama.Text
+        NPM = txtNPM.Text
+        Kehadiran = Val(txtKehadiran.Text)
+        Tugas = Val(txtTugas.Text)
+        UTS = Val(txtUTS.Text)
+        UAS = Val(txtUAS.Text)
+        Nilai = ((Kehadiran / 16) * 0.1 + Tugas * 0.2 + UTS * 0.3 + UAS * 0.4)
+        txtNilai.Text = Nilai
+
+        If Nilai >= 80 Then
+            Grade = "A"
+            txtBobot.Text = 4
+        ElseIf Nilai >= 70 Then
+            Grade = "B"
+            txtBobot.Text = 3
+        ElseIf Nilai >= 60 Then
+            Grade = "C"
+            txtBobot.Text = 2
+        ElseIf Nilai >= 50 Then
+            Grade = "D"
+            txtBobot.Text = 1
+        Else
+            Grade = "E"
+            txtBobot.Text = 0
+        End If
+        txtGrade.Text = Grade
+    End Sub
+
+    Private Sub BtnIPS_Click(sender As Object, e As EventArgs) Handles BtnIPS.Click
+        Dim Total As Double
+        Dim SKS As Double
+        Dim IPS As Double
+        Total = Val(txtTotal.Text)
+        SKS = Val(txtSKS.Text)
+        IPS = (Total / SKS)
+        txtIPS.Text = IPS
+    End Sub
+
+    Private Sub BtnIPK_Click(sender As Object, e As EventArgs) Handles BtnIPK.Click
+        Dim T_IPS As Double
+        Dim Semester As Double
+        Dim H_IPK As Double
+        T_IPS = Val(txtjumIPS.Text)
+        Semester = Val(txtSemester.Text)
+        H_IPK = (T_IPS / Semester)
+        txtHasilIPK.Text = H_IPK
+    End Sub
+End Class
